49621adf61d709383c1b0ed6a982cab357b29b74
Niall Cunningham <nccunningham@hotmail.com>
2018-11-16 15:27:00 +0000

    Update Main.java
    now funtional

--- C4.5/src/C45/Main.java
+++ C4.5/src/C45/Main.java
@@ -9,27 +9,27 @@ public class Main {
 
 		ArrayList<String> cata = new ArrayList<String>();
 		cata.add("red");
+		cata.add("red");
+		cata.add("red");
+		cata.add("red");
+		cata.add("blue");
+		cata.add("blue");
+		cata.add("blue");
 		cata.add("green");
-		cata.add("red");
-		cata.add("blue");
-		cata.add("red");
-		cata.add("blue");
-		cata.add("blue");
-		cata.add("red");
 		cata.add("green");
 		cata.add("green");
 
 		ArrayList<Integer> nums = new ArrayList<Integer>();
-		nums.add(1);
-		nums.add(5);
-		nums.add(2);
-		nums.add(10);
 		nums.add(0);
-		nums.add(8);
-		nums.add(9);
 		nums.add(1);
+		nums.add(2);
+		nums.add(2);
+		nums.add(5);
 		nums.add(5);
 		nums.add(6);
+		nums.add(8);
+		nums.add(9);
+		nums.add(10);
 
 		Node n = new Node(cata, nums);
 
@@ -40,20 +40,36 @@ public class Main {
 	public static void test(Node n) {
 
 		Collections.sort(n.getNums());
+		// Collections.sort(n.getCata());
 
 		int numberOfCata = n.getNumberOfCata();
 		ArrayList<String> cats = n.getUniqueCata();
+		int lowerThreshod = 0;
+		int higherThreshold = 0;
 
-		for (int j = 0; j < cats.size(); j++) {//get best in cat
-			
-			int min = 0;
+		for (int j = 0; j < cats.size(); j++) {// get best in cat
+
 			int max = n.getNums().size();
+			int min = higherThreshold;
+			int diection[] = getDirection(n, cats.get(j), min, max);			
+			float purity = 0;
 			
-			while (max > min) {
-				int correct = 0;
-				int inncorrect = 0;
 
-				for (int i = min; i < max; i++) {// ToDo iterate through range and choose left or right
+			int accuracy = 0;
+
+			while (min < max || accuracy < 100) {
+
+				
+				diection = getDirection(n, cats.get(j), min, max);
+				min = (min + diection[0]);
+				max = (max - diection[1]);
+
+				
+
+				int correct = 0;
+				int i = min;
+
+				while (i < max) {
 
 					if (n.getCata().get(i) == cats.get(j)) {
 
@@ -61,16 +77,82 @@ public class Main {
 
 					}
 
+					i++;
+
 				}
 
-				max--;
-				System.out.println("Catagory " + cats.get(j));//store
-				System.out.println("Correct: " + ((float) correct / (float) n.getNums().size() * 100) + "%\n");//store
+				float localPurity = correct / (float) (max - min);
+
+				if (localPurity > purity) {
+
+					purity = localPurity;
+					lowerThreshod = min;
+					higherThreshold = max;
+
+				}
+
+				// System.out.println("Catagory " + cats.get(j));
+				// System.out.println("Correct: " + ((float) localPurity * 100)
+				// + "%\n");// store
+				accuracy++;
 
 			}
+
+			System.out.println(cats.get(j) + ": " + purity * 100 + " correct\n" + n.getNums().get(lowerThreshod)
+					+ " lowerThreshod\n" + n.getNums().get(higherThreshold) + " lowerThreshod\n");
+
+		}
+
+	}
+
+	private static int[] getDirection(Node n, String cata, int min, int max) {// decides to reduce the thresholds min or max
+
+		ArrayList<String> catas = n.getCata();
+		ArrayList<Integer> nums = n.getNums();
+
+		int leftCorrect = 0;
+		int i = min;
+
+		while (i < max - 1) {
+
+			if (n.getCata().get(i) == cata) {
+
+				leftCorrect++;
+
+			}
+			
+			i++;
+
+		}
+
+		int rightCorrect = 0;
+		int j = min;
+		
+		while( j + 1 < max) {
+
+			if (n.getCata().get(j) == cata) {
+
+				rightCorrect++;
+
+			}
+
+			j++;
+			
+		}
+
+		if (rightCorrect >= leftCorrect) {
+
+			int[] direction = new int[] { 0, 1 };
+			return direction;
+
+		} else {
+
+			int[] direction = new int[] { 1, 0 };
+			return direction;
 
 		}
 
 	}
 
 }
+
