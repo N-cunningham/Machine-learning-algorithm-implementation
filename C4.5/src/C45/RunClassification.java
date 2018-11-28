dff855ec7f5eeb236acc65bf36b9363bc53721f6
Niall Cunningham <nccunningham@hotmail.com>
2018-11-28 18:48:43 +0000

    Add files via upload

---
+++ C4.5/src/C45/RunClassification.java
@@ -0,0 +1,19 @@
+package C45;
+
+import java.io.IOException;
+
+public class RunClassification {
+	
+	public static void main(String[] args) throws IOException{
+	
+	
+			
+		Score s = Main.loadData("F:/Downloads/owls (1).csv");
+		System.out.println(s.toString());
+			
+		
+		
+		
+	}
+
+}
