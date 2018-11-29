908f706de2ca66737e36e941ed2dd1eb6075e113
Niall Cunningham <nccunningham@hotmail.com>
2018-11-29 11:43:58 +0000

    Update Score.java

--- C4.5/src/C45/Score.java
+++ C4.5/src/C45/Score.java
@@ -7,12 +7,13 @@ public class Score {
 	
 	private int[] Thresholds;
 	private double informationGain;
+	private int attributeIndex;
 	
-
-	Score(int[] Thresholds, double informationGain){
+	Score(int[] Thresholds, double informationGain, int attributeIndex, Node n){
 		
 		this.Thresholds = Thresholds;
 		this.informationGain = informationGain;
+		this.attributeIndex = attributeIndex;
 		
 	}
 	
@@ -30,20 +31,16 @@ public class Score {
 		return informationGain;
 	}
 
-	@Override
-	public String toString() {
-	
-		return "Score [Thresholds=" + Arrays.toString(Thresholds) + ", informationGain=" + informationGain + "]";
-		
-	}
-
-
-
 	public void setInformationGain(double informationGain) {
 		this.informationGain = informationGain;
 	}
 
-	
+
+
+	@Override
+	public String toString() {
+		return "\nSplit on attribute " + attributeIndex + ": Score [Thresholds=" + Arrays.toString(Thresholds) + ", informationGain=" + informationGain + "]";
+	}
 
 
 }
