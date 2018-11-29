0f84693fd963decd6f1bbcdc6ab174c947d1c099
Niall Cunningham <nccunningham@hotmail.com>
2018-11-29 12:21:03 +0000

    Update RunClassification.java

--- C4.5/src/C45/RunClassification.java
+++ C4.5/src/C45/RunClassification.java
@@ -8,9 +8,14 @@ public class RunClassification {
 	
 	
 			
-		Score s = Main.loadData("F:/Downloads/owls (1).csv");
-		System.out.println(s.toString());
+		Main.loadData("F:/Downloads/owls (1).csv");
+		System.out.println(" ");
+		
+		for(int i = 0; i < Main.treeScores.size();i++){
 			
+			System.out.println(Main.treeScores.get(i).toString());
+			
+		}
 		
 		
 		
